290242acd906885d6fff07b295725d762ad69ae4
nguyenthuhang261 <nthnguyenthuhang2610@gmail.com>
2026-06-01 00:41:13 +0700

    Update FM.m

--- FM.m
+++ FM.m
@@ -1,6 +1,6 @@
-% Cho tín hiệu bản tin như sau:$$x(t) = \sin(8\pi t). Sử dụng Matlab để:
+% Cho tín hiệu bản tin như sau: x(t) = \sin(8\pi t). Sử dụng Matlab để:
 % a. Mô phỏng quá trình điều tần tín hiệu bản tin trên với sóng mang có các tham số như sau: 
-% tần số 1200 Hz, biên độ 1, pha ban đầu $\pi/2$, độ lệch tần số cực đại $\Delta f = 20\text{ Hz}$ (hoặc đề có thể cho chỉ số điều tần $\beta = 5$). 
+% tần số 1200 Hz, biên độ 1, pha ban đầu \pi/2, độ lệch tần số cực đại \Delta f = 20Hz(hoặc đề có thể cho chỉ số điều tần beta = 5). 
 % Chú ý: Không sử dụng hàm điều chế sẵn có trong Matlab.
 % b. Vẽ tín hiệu bản tin, tín hiệu sóng mang và tín hiệu sau điều tần trong khoảng 10 chu kì tín hiệu bản tin.
 
