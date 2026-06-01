8a88ce8a664daf7a13071a14dacb3371cf191ad0
nguyenthuhang261 <nthnguyenthuhang2610@gmail.com>
2026-06-01 15:56:47 +0700

    Create D23_DE3_CAU4.m

---
+++ D23_DE3_CAU4.m
@@ -0,0 +1,20 @@
+clear;
+clc;
+close all;
+
+n_bits = 200;
+% data = randi([0 1], 1, n_bits); % wrong
+data = randi([0 1], n_bits, 1); 
+
+M = 4;
+Nbs = log2(M);
+% data1 = bit2int(data, Nbs);
+data1 = bit2int(data, Nbs, true);
+dk = qammod(data1, M, 'gray');
+
+figure;
+plot(real(dk), imag(dk), 'bo', 'MarkerSize', 8);
+title('Gian do chom sao 4-QAM');
+xlabel('Truc thuc');
+ylabel('Truc ao');
+grid on;
