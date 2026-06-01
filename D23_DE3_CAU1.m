7a01b8b3f7d125cc64f6bbce1cbb30a9a5adf5d5
nguyenthuhang261 <nthnguyenthuhang2610@gmail.com>
2026-06-01 15:38:51 +0700

    Fix data generation and normalization in D23_DE3_CAU1.m

--- D23_DE3_CAU1.m
+++ D23_DE3_CAU1.m
@@ -1,17 +1,20 @@
 clear;
 clc;
 
-data = randi([0 3], 5, 6);
+% data = randi([0 3], 5, 6); % wrong
+data = 3*rand(5, 6);
 mean = 0;
 sigma = 3;
-data1 = sigma * randn([0 3], 5, 6);
+% data1 = sigma * randn([0 3], 5, 6); % wrong
+data1 = mean + sqrt(sigma) * randn(5, 6);
 A = data + 1i.*data1;
 fprintf("Ma tran A la: \n");
 disp(A);
 
 % b: Chuan hoa ma tran ve (0, 1)
 
-norm_a = abs(max(A));
+% norm_a = abs(max(A)); % wrong
+norm_a = max(abs(A(:)));
 A_norm = A.*(1/norm_a);
 fprintf("Ma tran A sau chuan hoa la: \n");
 disp(A_norm);
