ffea99cf160b120dae83950cab3c7256f781c6a9
nguyenthuhang261 <nthnguyenthuhang2610@gmail.com>
2026-06-01 14:59:22 +0700

    Create D23_DE3_CAU2.m

---
+++ D23_DE3_CAU2.m
@@ -0,0 +1,20 @@
+clear;
+clc;
+
+xa = 0;
+xb = 2;
+f = @(x) cos(x) * exp(-x);
+N = 50;
+
+res = mid_point(f, xa, xb, N)
+fprintf("Gia tri cua tich phan la: %.6f \n", res);
+
+function res = mid_point(f, xa, xb, N)
+  h = abs(xb - xa)/N;
+  x = xa:h:xb;
+  k = 1;
+  res = 0;
+  while k <= N
+      res = res + f(x(k) + h/2) * h;
+  end
+end
